bbdf61aeebe2de994b8bd4eac71ff6bd37b3e59f
sahil2132 <sahil.sachdeva@SAs.Dlink>
2017-04-26 22:07:28 +0530

    Added a Dummy WebView and child count of current activity

--- app/src/main/java/android/catalyst/com/testapp/MainActivity.java
+++ app/src/main/java/android/catalyst/com/testapp/MainActivity.java
@@ -3,7 +3,10 @@ package android.catalyst.com.testapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void goToActivity2(View view){
-        Intent intent2  = new Intent(this,Activity2.class);
-        startActivity(intent2);
+        View rootView = this.findViewById(android.R.id.content).getRootView();
+        Log.wtf("ROOT VIEW",rootView.toString());
+
+        ViewGroup viewGroup = (ViewGroup) view.getParent();
+        Log.wtf("ROOT VIEW",viewGroup.getChildCount()+"");
+
+//        WebView webView1 = (WebView) findViewById(R.id.webView);
+//        webView1.getSettings().setJavaScriptEnabled(true);
+//        webView1.loadUrl("http://www.google.co.in/");
+//        Intent intent2  = new Intent(this,Activity2.class);
+//        startActivity(intent2);
     }
 }
